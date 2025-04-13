4068f77aec9addc40d8e7a0c5a59b364884f60f5
DavidGilWeb <info@davidgilweb.com>
2025-04-13 13:33:47 +0200

    useEffect hook

--- src/components/ToggleButton.jsx
+++ src/components/ToggleButton.jsx
@@ -1,7 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const ToggleButton = () => {
 const [isActive, setIsActive] = useState(false);
+
+useEffect(() => {
+    console.log(`Button is ${isActive ? 'active' : 'inactive'}`);
+});
 
 return (
     <button onClick={() => setIsActive(!isActive)}>{isActive ? 'Active' :'Inactiive'}</button>
